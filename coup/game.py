ccaf883741c08a924536e047c3e47c6964f863b7
AlexanderHOtt <aott01@protonmail.ch>
2023-08-02 23:58:31 -0700

    narrow scope for mpv

--- coup/game.py
+++ coup/game.py
@@ -1,20 +1,18 @@
 from random import shuffle
+import copy
 from dataclasses import dataclass
 
 from coup.player import Player
-from coup.game_state import Card, GameState, Action
+from coup.action import Card, Action
 
-
-@dataclass
-class GameState:
-    players: list[Player]
-    active_player: Player
-    revealed_deck: list[Card]
+Cuid2 = str
 
 
 class Game:
     def __init__(self, players: list[Player]) -> None:
         self.players = players
+        self.current_player_idx = 0
+        self.perfect_game_states: list["GameState"] = []
 
         self.deck = [
             Card.AMBASSADOR,
@@ -48,29 +46,80 @@ class Game:
             return alive_players[0]
         return None
 
-    def get_perfect_state(self) -> GameState:
-        ...
-
-    def get_state(self, player: Player) -> GameState:
-        ...
-
     def play(self):
-        for player in self.players:
-            action = player.ask_action(self.get_legal_actions(player))
-            # new_state = self.update(action)
+        self.perfect_game_states.append(GameState.from_game_perfect(self))
+        while self.is_over() is None:
+            for i, player in enumerate(self.players):
+                [print(player) for player in self.players]
+                [print(s) for s in self.perfect_game_states]
+                self.current_player_idx = i
+                # self.player_game_states[player.id].append(
+                #     GameState.for_player(self, player)
+                # )
 
-            for p in self.players:
-                if player == p:
-                    continue
+                action = player.ask_action(self.get_legal_actions(player))
 
-                res = p.ask_action(self.get_legal_actions(p, action))
-                if res == Action.NOTHING:
-                    continue
+                print("pre ")
+                self.update_gamestate(action, self.current_player_idx)
+                print("players coins", self.players[i].coins)
 
-                ...
+                # Ask to
+                highest_action = Action.NOTHING
+                for p in self.players:
+                    if player == p:
+                        continue
 
-    def update(self, action: Action) -> GameState:
-        ...
+                    res = p.ask_action(self.get_legal_actions(p, action))
+                    if res == Action.NOTHING:
+                        continue
+
+                    highest_action = res
+                    break
+
+                self.update_gamestate(highest_action, self.current_player_idx)
+
+    def update_gamestate(self, action: Action, player_idx: int):
+        state = copy.deepcopy(
+            self.perfect_game_states[-1]
+            if len(self.perfect_game_states)
+            else GameState.from_game_perfect(self, action)
+        )
+        state.current_player = player_idx
+        print(f"Updating state: p: {state.last_action} c: {action}")
+
+        if state.last_action == Action.NOTHING:
+            state.last_action = action
+        # if state.last_action == Action.NOTHING and action == Action.INCOME:
+        #     state.last_action = action
+        # elif state.last_action == Action.NOTHING and action == Action.STEAL:
+        #     state.last_action = action
+        # elif state.last_action == Action.NOTHING and action == Action.TAX:
+        #     ...
+
+        if state.last_action == Action.INCOME and action == Action.NOTHING:
+            self.players[player_idx].coins += 1
+            state.last_action = action
+            state.players[player_idx].coins += 1
+
+        elif state.last_action == Action.TAX and action == Action.NOTHING:
+            print(
+                "taxing"
+            )
+            self.players[player_idx].coins += 3
+            state.last_action = action
+            state.players[player_idx].coins += 3
+
+        elif state.last_action == Action.COUP and action == Action.NOTHING:
+            other_player_idx = 0 if player_idx == 1 else 1
+            self.players[other_player_idx].lose_influence()
+            ...
+
+
+        elif state.last_action == Action.STEAL and action == Action.BLOCK_STEAL:
+            ...
+
+        self.perfect_game_states.append(state)
+        # return state
 
     def get_legal_actions(
         self, player: Player, prev_action: Action = Action.NOTHING
@@ -124,3 +173,70 @@ class Game:
             return [Action.NOTHING, Action.CHALLENGE_BLOCK_ASSASSINATE]
 
         raise ValueError(f"{prev_action} does not have any valid responses")
+
+
+@dataclass
+class PlayerState:
+    hand: list[int]
+    dead: list[int]
+    coins: int
+
+    @staticmethod
+    def from_player_perfect(player: Player) -> "PlayerState":
+        hand = list(map(lambda c: c.value, player.hand))
+        while len(hand) > 2:
+            hand.append(Card.EMPTY.value)
+
+        dead = list(map(lambda c: c.value, player.dead))
+        while len(dead) > 2:
+            dead.append(Card.EMPTY.value)
+
+        return PlayerState(hand=hand, dead=dead, coins=player.coins)
+
+    @staticmethod
+    def from_player_blind(player: Player) -> "PlayerState":
+        hand = list(map(lambda _: Card.UNKNOWN.value, player.hand))
+        while len(hand) > 2:
+            hand.append(Card.EMPTY.value)
+
+        dead = list(map(lambda _: Card.UNKNOWN.value, player.hand))
+        while len(dead) > 2:
+            dead.append(Card.EMPTY.value)
+
+        return PlayerState(hand=hand, dead=dead, coins=player.coins)
+
+
+@dataclass
+class GameState:
+    players: list[PlayerState]
+    current_player: int
+    last_action: Action
+
+    @staticmethod
+    def from_game_perfect(
+        game: Game, last_action: Action = Action.NOTHING
+    ) -> "GameState":
+        player_states = []
+        for player in game.players:
+            player_states.append(PlayerState.from_player_perfect(player))
+
+        return GameState(
+            players=player_states,
+            current_player=game.current_player_idx,
+            last_action=last_action,
+        )
+
+    @staticmethod
+    def for_player(game: Game, player: Player, last_action: Action) -> "GameState":
+        player_states = []
+        for p in game.players:
+            if p == player:
+                player_states.append(PlayerState.from_player_perfect(player))
+            else:
+                player_states.append(PlayerState.from_player_blind(player))
+
+        return GameState(
+            players=player_states,
+            current_player=game.current_player_idx,
+            last_action=last_action,
+        )
--- coup/player.py
+++ coup/player.py
@@ -1,4 +1,8 @@
-from coup.game_state import Card, Action
+from coup.action import Card, Action
+
+from cuid2 import cuid_wrapper
+
+id_generator = cuid_wrapper()
 
 
 class Player:
@@ -8,13 +12,33 @@ class Player:
         else:
             self.hand = hand
 
-        self.face_up = []
+        self.dead = [Card.EMPTY, Card.EMPTY]
         self.coins = coins
+        self.id = id_generator()
 
     def ask_action(self, legal_actions: list[Action]) -> Action:
         if len(legal_actions) == 1:
             return legal_actions[0]
 
-        print(legal_actions)
-        i = int(input("Select action index"))
-        return legal_actions[i]
+        for i in range(len(legal_actions)):
+            print(f"{i}: {legal_actions[i].name}")
+
+        selection = int(input("Select action index: "))
+        return legal_actions[selection]
+
+    def lose_influence(self):
+        if len(self.hand) == 1:
+            return self.hand[0]
+
+        for i in range(len(self.hand)):
+            print(f"{i}: {self.hand[i].name}")
+
+        selection = int(input("Select card index (to lose): "))
+
+        self.dead.append(self.hand[selection])
+        self.dead.remove(Card.EMPTY)
+        self.hand.pop(selection)
+        self.hand.append(Card.EMPTY)
+    
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(hand={self.hand}, coins={self.coins})"
